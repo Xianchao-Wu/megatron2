572514eefb722f9f787c631f237cb989b0ccc310
Xianchao Wu <wuxianchao@gmail.com>
2021-02-14 16:38:33 +0000

    a bug of tokenizer-type

--- tools/preprocess_data.py
+++ tools/preprocess_data.py
@@ -132,7 +132,7 @@ def get_args():
     group = parser.add_argument_group(title='tokenizer')
     group.add_argument('--tokenizer-type', type=str, required=False,
                        default='BertWordPieceJp', # for japanese bert; #'BertWordPieceLowerCase' for english bert,
-                       choices=['BertWordPieceLowerCase','BertWordPieceCase', 'BertWordPieceJp'
+                       choices=['BertWordPieceLowerCase','BertWordPieceCase', 'BertWordPieceJp',
                                 'GPT2BPETokenizer'],
                        help='What type of tokenizer to use.')
     group.add_argument('--vocab-file', type=str, 
