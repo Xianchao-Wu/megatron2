ea49366cf2aef5c5f9c14c2601c73be45b3dc98b
Xianchao Wu <wuxianchao@gmail.com>
2021-02-14 17:06:25 +0000

    for 6 and 7 fin bert on en jp prepare

---
+++ examples/7.pretrain_bert_distributed_dataprep_jp.sh
@@ -0,0 +1,26 @@
+
+#jsonfile="/workspace/megatron/ngc_models/bert_pretrain/small_data.json"
+#jsonfile="/workspace/megatron/ngc_models/bert_pretrain/small_data_line1.json" # TODO work
+#jsonfile="/workspace/megatron/ngc_models/bert_pretrain/small_data_line2.json" # TODO
+
+#jsonfile="/workspace/megatron/ngc_models/bert_pretrain/small_data_line3.json"
+#prefix="my-bert"
+
+#jsonfile="/workspace/megatron/datasets/english-fsi/temp.testin.json"
+#jsonfile="/workspace/megatron/datasets/english-fsi/eight.files.json"
+#vocabfile="/workspace/megatron/ngc_models/bert-large-cased-vocab.txt"
+#prefix="fsi-en-bert-8files-bert-large-cased-vocab-bwplc"
+
+# TODO actually 8files are actually 12 files: mainichi, nikkei, and sankei
+jsonfile="/workspace/megatron/datasets/japanese-fsi/jp.fsi.8files.json"
+vocabfile="/workspace/megatron/datasets/japanese-fsi/jp.fsi.8files.txt.vocab"
+prefix="fsi-ja-bert-12files-bert-large-vocab-bwplc"
+
+python tools/preprocess_data.py \
+	       --input $jsonfile \
+	       --output-prefix $prefix \
+	       --vocab $vocabfile \
+	       --dataset-impl mmap \
+	       --tokenizer-type BertWordPieceJp #\
+	       #--tokenizer-type BertWordPieceLowerCase #\
+	       #--split-sentences # TODO no need anymore, default is perform split sent
